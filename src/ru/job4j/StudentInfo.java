f42b92a78aea023d14c6270fa098f9bfd5b72bf4
Sles Ilia <slesilya@mail.ru>
2021-06-24 00:46:30 +0300

    Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ilya Sles");
+        System.out.println("24.01.1992");
+    }
+}

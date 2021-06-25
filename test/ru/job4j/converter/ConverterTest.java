2453f0caae0fd29ee1a56e0a6ce2050ee86fdf87
Sles Ilia <slesilya@mail.ru>
2021-06-26 00:37:30 +0300

    Модульные тесты. [#159435 #171487]

--- test/ru/job4j/converter/ConverterTest.java
+++ test/ru/job4j/converter/ConverterTest.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 public class ConverterTest {
     @Test
     public void whenConvert560RblThen8Euro() {
-        int in = 560;
-        int expected = 8;
+        int in = 140;
+        int expected = 2;
         int out = Converter.rubleToEuro(in);
         Assert.assertEquals(expected, out);
     }
 
     @Test
     public void whenConvert560RblThen9Dlr() {
-        int in = 560;
-        int expected = 9;
+        int in = 180;
+        int expected = 3;
         int out = Converter.rubleToDollar(in);
         Assert.assertEquals(expected, out);
     }
